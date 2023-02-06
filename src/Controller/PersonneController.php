cf8a4f45e7017226e706857da9dcfae428e1b2f0
bensalah-nabil <bensalah.nabil@etudiant-fst.utm.tn>
2023-02-06 19:34:48 +0100

    doctrine + controller

--- src/Controller/PersonneController.php
+++ src/Controller/PersonneController.php
@@ -2,17 +2,31 @@
 
 namespace App\Controller;
 
+use App\Entity\Personne;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class PersonneController extends AbstractController
 {
-    #[Route('/personne', name: 'app_personne')]
-    public function index(): Response
+    #[Route('/personne/add', name: 'app_personne')]
+    public function addPersonne(ManagerRegistry $doctrine): Response
     {
-        return $this->render('personne/index.html.twig', [
-            'controller_name' => 'PersonneController',
+        $entityManager = $doctrine->getManager();
+        $personne = new  Personne();
+        $personne->setFirstname('Nabil');
+        $personne->setName('Ben Salah');
+        $personne->setAge(24);
+        $personne->setAddress('Kebili');
+
+        //Ajouter l'operation de la personne dans ma transaction
+        $entityManager->persist($personne);
+
+        //Execute this transaction Todo
+        $entityManager->flush();
+        return $this->render(' personne/detail.html.twig', [
+            'personne' => $personne,
         ]);
     }
 }
