3106841d8836373e2cc25cfc6a12f710d4bda7e0
bensalah-nabil <bensalah.nabil@etudiant-fst.utm.tn>
2023-02-07 00:26:53 +0100

    Doctrine findall()

--- src/Controller/PersonneController.php
+++ src/Controller/PersonneController.php
@@ -4,13 +4,23 @@ namespace App\Controller;
 
 use App\Entity\Personne;
 use Doctrine\Persistence\ManagerRegistry;
+use Faker\Guesser\Name;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route('personne')]
 class PersonneController extends AbstractController
 {
-    #[Route('/personne/add', name: 'app_personne')]
+    #[Route('/',name: 'personne.list')]
+    public function index(ManagerRegistry $doctrine) : Response
+    {
+       $repository = $doctrine->getRepository(Personne::class);
+       $personnes = $repository->findAll();
+       return $this->render('personne/index.html.twig',
+           ['personnes'=>$personnes]);
+    }
+    #[Route('/add', name: 'personne.add')]
     public function addPersonne(ManagerRegistry $doctrine): Response
     {
         $entityManager = $doctrine->getManager();
