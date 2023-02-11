c8f52e30c2f83dc8469009fb36d90a61f173921b
bensalah-nabil <bensalah.nabil@etudiant-fst.utm.tn>
2023-02-11 19:30:41 +0100

    doctrine findBy() + Pagination

--- src/Controller/PersonneController.php
+++ src/Controller/PersonneController.php
@@ -19,6 +19,25 @@ class PersonneController extends AbstractController
         return $this->render('personne/index.html.twig',
             ['personnes'=>$personnes]);
     }
+    #[Route('/all/{page?1}/{nber?12}',name: 'personne.list.all')]
+    public function indexAll(ManagerRegistry $doctrine, $page, $nber) : Response
+    {
+        $repository = $doctrine->getRepository(Personne::class);
+        $nbPersonne = $repository->count([]);
+        $nbPage = ceil($nbPersonne / $nber );
+
+        $personnes = $repository->findBy([],[],limit: $nber,offset: ( $page - 1 ) * $nber);
+
+        return $this->render('personne/index.html.twig',
+            [
+                'personnes'=>$personnes,
+                'isPaginated' => True,
+                'nbPage' => $nbPage,
+                'page' =>$page,
+                'nber' => $nber,
+            ]);
+    }
+
     #[Route('/{id<\d+>}',name: 'personne.detail')]
     public function detail(Personne $personne = null) : Response
     {
@@ -29,7 +48,6 @@ class PersonneController extends AbstractController
         return $this->render('personne/detail.html.twig',
             ['personne'=>$personne]);
     }
-
     #[Route('/add', name: 'personne.add')]
     public function addPersonne(ManagerRegistry $doctrine): Response
     {
